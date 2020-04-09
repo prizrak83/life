40be0a15f127235d104a7e256095f57aaf888dff
prizrak83 <prizrak83@gmail.com>
2020-04-09 11:20:20 +0300

    тестирование производительности

--- Main.py
+++ Main.py
@@ -1,12 +1,18 @@
 import numpy as np
 
-FILDH = 10
-FILDW = 10
+FILDH = 100
+FILDW = 100
 
 def main(filda,fildb):
-    printfild(filda)
-    checkfild(filda, fildb)
-    printfild(fildb)
+    s = 500
+    while s>0 :
+#        printfild(filda)
+        checkfild(filda, fildb)
+#        printfild(fildb)
+        checkfild(fildb, filda)
+        s -= 1
+        print(s)
+#    printfild(filda)
 
 
 def checkfild(fild1, fild2):
@@ -15,11 +21,11 @@ def checkfild(fild1, fild2):
             fild2[str, s] = nextturn(fild1, str, s)
 
 
-
 def nextturn(fild, str, s):
     if sosedi(fild, str, s) == 3 or ((sosedi(fild, str, s) == 2) and fild[str, s]):
         return True
     return False
+
 
 def sosedi(fild, str, s):
     return (int(fild[str-1, s-1])+int(fild[str-1, s])+int(fild[str-1, s+1])+
@@ -30,15 +36,22 @@ def printfild(fild):
     for str in range(1, FILDW+1):
 
         for s in range(1,FILDH+1):
-            #print(1 if fild[str, s] else 0, sosedi(fild, str, s), sep='', end=' ')
             print(1 if fild[str, s] else 0, end=' ')
         print('')
+    print(' ')
 
 
-filda = np.zeros((FILDW+3, FILDH+3), dtype = bool)
-fildb = np.zeros((FILDW+3, FILDH+3), dtype = bool)
-filda[5, 5] = True
-filda[5, 6] = True
-filda[5, 7] = True
+filda = np.zeros((FILDW+3, FILDH+3), dtype=bool)
+fildb = np.zeros((FILDW+3, FILDH+3), dtype=bool)
 
-main(filda,fildb)
+filda[3, 1] = True
+filda[3, 2] = True
+filda[3, 3] = True
+filda[2, 3] = True
+filda[1, 2] = True
+
+filda[2, 7] = True
+filda[2, 8] = True
+filda[2, 9] = True
+
+main(filda, fildb)
