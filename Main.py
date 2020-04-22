a292c69234cf0de272e6a1dfecb916d1c0c6fcf6
prizrak83 <prizrak83@gmail.com>
2020-04-22 19:47:03 +0300

    Чистка мусора

--- Main.py
+++ Main.py
@@ -1,54 +1,45 @@
 import numpy as np
-import time
 import tkinter as tk
 
 SIZE = 100
 
 
-def main(filda,fildb, fildcanvas,  step):
+def main(filda,fildb, fildcanvas,  step, canvas):
     step += 1
     drowfild(filda, fildcanvas)
     checkfild(filda, fildb)
     drowfild(fildb, fildcanvas)
     checkfild(fildb, filda)
-    canvas.after(10,main(filda, fildb, fildcanvas, step))
+    canvas.after(1, main(filda, fildb, fildcanvas, step, canvas))
 
 def drowfild(fild, fildcanvas):
-    for str in range(1, SIZE + 1):
-        for s in range(1, SIZE + 1):
-            if fild[str,s]:
-                canvas.itemconfig(fildcanvas[str, s], fill="blue")
+    for i in range(1, SIZE + 1):
+        for j in range(1, SIZE + 1):
+            if fild[i,j]:
+                canvas.itemconfig(fildcanvas[i, j], fill="blue")
             else:
-                canvas.itemconfig(fildcanvas[str, s], fill="white")
+                canvas.itemconfig(fildcanvas[i, j], fill="white")
     canvas.update()
 
 
-def checkfild(fild1, fild2):
-    for str in range(1, SIZE):
-        for s in range(1, SIZE):
-            fild2[str, s] = nextturn(fild1, str, s)
+def checkfild(fild_old, fild_new):
+    for i in range(1, SIZE):
+        for j in range(1, SIZE):
+            fild_new[i, j] = nextturn(fild_old, i, j)
 
 
-def nextturn(fild, str, s):
-    sosedi_kletki = sosedi(fild, str, s)
-    if sosedi_kletki == 3 or ((sosedi_kletki == 2) and fild[str, s]):
+def nextturn(fild, i, j):
+    neighbours = neighbour(fild, i, j)
+    if neighbours == 3 or ((neighbours == 2) and fild[i, j]):
         return 1
     return 0
 
 
 
-def sosedi(fild, str, s):
-    return (fild[str-1, s-1] + fild[str-1, s] + fild[str-1, s+1] + fild[str, s-1] + fild[str, s+1] +
-            fild[str+1, s-1] + fild[str+1, s] + fild[str+1, s+1])
+def neighbour(fild, i, j):
+    return (fild[i-1, j-1] + fild[i-1, j] + fild[i - 1, j + 1] + fild[i, j - 1] + fild[i, j + 1] +
+            fild[i + 1, j - 1] + fild[i + 1, j] + fild[i + 1, j + 1])
 
-
-def printfild(fild):
-    for str in range(1, SIZE + 1):
-
-        for s in range(1, SIZE + 1):
-            print(1 if fild[str, s] else 0, end=' ')
-        print('')
-    print(' ')
 
 
 root = tk.Tk()
@@ -61,11 +52,11 @@ canvas.pack()
 filda = np.zeros((SIZE + 3, SIZE + 3), dtype=int)
 fildb = np.zeros((SIZE + 3, SIZE + 3), dtype=int)
 fildcanvas = np.zeros((SIZE+3, SIZE+3), dtype=int)
-str=1
-s=1
-for str in range(1, SIZE + 1):
-    for s in range(1, SIZE + 1):
-        fildcanvas[str, s] = canvas.create_oval(str * 10 - 5, s * 10 - 5, str * 10 + 5, s * 10 + 5, outline="white")
+
+i, j = 1, 1
+for i in range(1, SIZE + 1):
+    for j in range(1, SIZE + 1):
+        fildcanvas[i, j] = canvas.create_oval(i * 10 - 5, j * 10 - 5, i * 10 + 5, j * 10 + 5, outline="white")
 
 
 
@@ -79,6 +70,6 @@ filda[54, 51] = 1
 filda[53, 50] = 1
 filda[54, 50] = 1
 step =1
-main(filda, fildb, fildcanvas, step)
+main(filda, fildb, fildcanvas, step, canvas)
 
 root.mainloop()
