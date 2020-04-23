22310e10f5682d6ab94d935492b3350ee1dbbf1b
prizrak83 <prizrak83@gmail.com>
2020-04-23 12:39:44 +0300

    Добавление управления через меню

--- Main.py
+++ Main.py
@@ -1,16 +1,19 @@
 import numpy as np
 import tkinter as tk
 
+
 SIZE = 100
+FACTOR = 8
 
 
-def main(filda,fildb, fildcanvas,  step, canvas):
+def main(filda,fildb, fildcanvas,  canvas):
+    global step
     step += 1
-#    drowfild(filda, fildcanvas)
     checkfild(filda, fildb, fildcanvas, canvas)
-#    drowfild(fildb, fildcanvas)
+    root.title('Life' + '   ' + str(step))
+    step += 1
     checkfild(fildb, filda, fildcanvas, canvas)
-    canvas.after(1, main(filda, fildb, fildcanvas, step, canvas))
+    root.title('Life' + '   ' + str(step))
 
 def drowfild(fild, fildcanvas):
     for i in range(1, SIZE + 1):
@@ -49,10 +52,41 @@ def neighbour(fild, i, j):
             fild[i + 1, j - 1] + fild[i + 1, j] + fild[i + 1, j + 1])
 
 
+def quit():
+  global run, quit_flag
+  if not(run):
+    root.destroy()
+  else:
+    run = False
+    quit_flag = True
 
+def start_stop():
+  global run, quit_flag ,start_flag
+  run = not(run)
+  if run:
+    mainmenu.entryconfig(1, label='stop')
+  else:
+    mainmenu.entryconfig(1, label='start')
+
+  while run:
+    main(filda, fildb, fildcanvas, canvas)
+  else:
+    if quit_flag:
+      root.destroy()
+
+
+run = False
+quit_flag = False
+step =1
 root = tk.Tk()
-root.title = 'test'
-canvas = tk.Canvas(root, width=SIZE*10, height=SIZE*10)
+root.title('Life'+ '   ' + str(step))
+canvas = tk.Canvas(root, width=SIZE*FACTOR, height=SIZE*FACTOR)
+mainmenu=tk.Menu(root)
+
+root.config(menu=mainmenu)
+mainmenu.add_command(label='start', command=start_stop)
+mainmenu.add_command(label='exit', command=quit)
+
 canvas.pack()
 
 
@@ -62,12 +96,13 @@ fildb = np.zeros((SIZE + 3, SIZE + 3), dtype=int)
 fildcanvas = np.zeros((SIZE+3, SIZE+3), dtype=int)
 
 i, j = 1, 1
+oval_size = FACTOR//2
 for i in range(1, SIZE + 1):
     for j in range(1, SIZE + 1):
-        fildcanvas[i, j] = canvas.create_oval(i * 10 - 5, j * 10 - 5, i * 10 + 5, j * 10 + 5,
+        fildcanvas[i, j] = canvas.create_oval(i * FACTOR - oval_size, j * FACTOR - oval_size,
+                                              i * FACTOR + oval_size, j * FACTOR + oval_size,
                                               outline="white", fill = "white")
 
-canvas.update()
 
 
 filda[49, 51] = 1
@@ -77,7 +112,8 @@ filda[51, 50] = 1
 filda[50, 49] = 1
 drowfild(filda, fildcanvas)
 canvas.update()
-step =1
-main(filda, fildb, fildcanvas, step, canvas)
+
+
+
 
 root.mainloop()
